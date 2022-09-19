a945044e27ff28ad7b9ab0b67cc08e095f9200a6
anziv <anziv@LAPTOP-HH5Q2VJ1>
2022-09-19 10:20:41 -0500

    FIRST COMMIT

---
+++ TESTING/src/Addition/Static.java
@@ -0,0 +1,31 @@
+package Addition;
+
+public class Static {
+  static int a=10;
+  static int b=5;
+  
+ 
+  
+  
+  public static void main(String[]args)
+  {
+	  int c=a+b;
+	  System.out.print("sum is "+c);
+
+  }
+  
+	  }
+	  
+	  
+
+
+		
+
+		
+	
+		
+	
+
+
+
+
